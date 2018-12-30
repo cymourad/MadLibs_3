c698ae17274b57450e7e27733bd5d0cbd521bccc
Cyril <cyrilfortnite2007@gmail.com>
2018-12-30 17:40:44 -0500

    First Commit

---
+++ MadLibs_3/MadLibs_3.py
@@ -0,0 +1,14 @@
+color = input("Enter a color:")
+youtuber = input("Enter a youtuber:")
+videogame = input("Enter a videogame:")
+car_brand = input("Enter a car brand:")
+chocolate_brand = input("Enter a chocalate brand:")
+country_name_in_europe = input("Enter a country name in Europe:")
+
+print("I like " + color)
+print("This youtuber said: subscribe to " + youtuber)
+print("Let's play some videogames, " + videogame + " is the best.")
+print("I have a " + car_brand + " ,is the best!")
+print("Let's buy some chocalate, " + chocolate_brand + " tastes good.")
+print("I went to " + country_name_in_europe + " before, it is masive!")
+
